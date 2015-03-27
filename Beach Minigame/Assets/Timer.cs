be1a7c9a5d7591977e3cb52b7247f88d922e6525
Neel Balusa <neelbalusa@gmail.com>
2015-03-27 16:18:42 -0700

    Made a Timer-Neel

---
+++ Beach Minigame/Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float startTime;
+	private string currentTime;
+	
+	public Rect timerRect;
+	public GUISkin skin;
+	
+	void Update()
+	{
+		startTime -= Time.deltaTime;
+		currentTime = string.Format("{0:0.0}",startTime);
+		if (startTime <= 0) 
+		{
+			startTime = 0;
+			Application.Quit();
+		}
+		
+	}
+	void OnGUI()
+	{
+		GUI.Label (timerRect, currentTime, skin.GetStyle("Timer"));
+	}
+}
+
