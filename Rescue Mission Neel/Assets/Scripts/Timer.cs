1155b17205ade36e4be709697c2cd45a25c6b175
Neel Balusa <neelbalusa@gmail.com>
2015-03-27 15:06:27 -0700

    added timer and footsteps

---
+++ Rescue Mission Neel/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	public float startTime;
+	private string currentTime;
+
+	public Rect timerRect;
+	public GUISkin skin;
+
+	void Update()
+	{
+		startTime -= Time.deltaTime;
+		currentTime = startTime.ToString ();
+		if (startTime <= 0) 
+		{
+			startTime = 0;
+			Application.Quit();
+		}
+
+	}
+	void OnGUI()
+	{
+		GUI.Label (timerRect, currentTime);
+	}
+}
