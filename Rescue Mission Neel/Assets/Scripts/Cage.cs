d1521b0e074fa95832b9d87b18f059f18e90bc9b
Neel Balusa <neelbalusa@gmail.com>
2015-03-27 11:42:23 -0700

    Moving the code to rescue mission - neel

---
+++ Rescue Mission Neel/Assets/Scripts/Cage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioClip))]
+public class Cage : MonoBehaviour {
+	public AudioClip unlock;
+
+	void OnTriggerEnter(Collider co1)
+	{
+		if(co1.gameObject.tag == "Cage")		
+		{
+			print ("Destroyed");
+			Destroy(co1.gameObject);
+			GetComponent<AudioSource>().PlayOneShot(unlock,1);
+		}
+	}
+}
