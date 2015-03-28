8d4165494e1247ad7c89e99f366ec88a51909a94
Neel Balusa <neelbalusa@gmail.com>
2015-03-27 19:25:45 -0700

    added ticking noise

---
+++ Rescue Mission/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	public float Minutes;
+	private float seconds = 0;
+	private string currentTime;
+
+	public Rect timerRect;
+	public GUISkin skin;
+
+	void Update()
+	{
+
+		if (seconds <= 0) {
+			Minutes--;
+			seconds = 59;
+		}
+		seconds -= Time.deltaTime;
+		currentTime = string.Format("{0:0}:{1:0}",Minutes,seconds);
+		if (Minutes <= 0) 
+		{
+			Minutes = 0;
+			seconds = 0;
+
+		}
+
+	}
+	void OnGUI()
+	{
+		GUI.Label (timerRect, currentTime, skin.GetStyle("Timer"));
+	}
+}
---
+++ Rescue Mission/Assets/Scripts/Victim.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Victim : MonoBehaviour {
+	public float moveSpeed;
+	public Transform player;
+	private bool isFollowing;
+	
+	void Start()
+	{
+		isFollowing = false;
+	}
+	void OnTriggerEnter(Collider co)
+	{
+		if (co.tag == "Player") 
+		{
+			isFollowing = true;
+		}
+	}
+	void Update()
+	{
+		if (isFollowing == true) {
+			transform.position = Vector3.MoveTowards (transform.position, player.position, moveSpeed * Time.deltaTime);
+			transform.LookAt(player);
+		} 
+		else 
+		{
+		
+		}
+	}
+}
